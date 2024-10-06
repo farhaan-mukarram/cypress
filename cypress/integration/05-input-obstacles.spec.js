26354e6a6a2e2138035a733c98d86f0104651897
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-10-07 00:16:55 +0500

    feat: add solution for exercise 5

--- cypress/integration/05-input-obstacles.spec.js
+++ cypress/integration/05-input-obstacles.spec.js
@@ -13,38 +13,40 @@ describe('Input obstacles', () => {
   });
 
   it('should control a select input', () => {
-    cy.get('[data-test="select-input"]');
-    cy.get('[data-test="select-result"]');
+    cy.get('[data-test="select-input"]').select('Thor');
+
+    cy.get('[data-test="select-result"]').contains('Thor');
   });
 
   it('should find and control a checkbox input', () => {
-    cy.get('[data-test="checkbox-tomato"]');
     cy.get('[data-test="checkbox-result"]').contains('(None)');
-    cy.get('[data-test="checkbox-result"]');
+
+    cy.get('[data-test="checkbox-tomato"]').check();
+    cy.get('[data-test="checkbox-result"]').contains('Tomato');
   });
 
   it('should find and control a radio input', () => {
-    cy.get('[data-test="radio-ringo"]');
-    cy.get('[data-test="radio-result"]');
+    cy.get('[data-test="radio-ringo"]').check();
+    cy.get('[data-test="radio-result"]').contains('Ringo');
   });
 
   it('should find and control a color input', () => {
-    cy.get('[data-test="color-input"]');
-    cy.get('[data-test="color-result"]');
+    cy.get('[data-test="color-input"]').invoke('val', '#000000').trigger('input');
+    cy.get('[data-test="color-result"]').contains('#000000');
   });
 
   it('should find and control a date input', () => {
-    cy.get('[data-test="date-input"]');
-    cy.get('[data-test="date-result"]');
+    cy.get('[data-test="date-input"]').invoke('val', '2022-06-14').trigger('input');
+    cy.get('[data-test="date-result"]').contains('2022-06-14');
   });
 
   it('should find and control a range input', () => {
-    cy.get('[data-test="range-input"]');
-    cy.get('[data-test="range-result"]');
+    cy.get('[data-test="range-input"]').invoke('val', '5').trigger('change');
+    cy.get('[data-test="range-result"]').contains('5');
   });
 
   it('should find and control a file input', () => {
-    cy.get('[data-test="file-input"]');
-    cy.get('[data-test="file-result"]');
+    cy.get('[data-test="file-input"]').selectFile('cypress/fixtures/file.json');
+    cy.get('[data-test="file-result"]').contains('file.json');
   });
 });
