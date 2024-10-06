a5b2dc966b839c8ba53af5d19982a3635d58fc5a
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-10-06 14:13:12 +0500

    feat: add solution for exercise 4

--- cypress/integration/04-other-inputs.spec.js
+++ cypress/integration/04-other-inputs.spec.js
@@ -8,9 +8,40 @@ describe('Secret Menu Items', () => {
     cy.get('#restaurant-visibility-filter').as('restaurant-filter');
   });
 
-  it('should set the range and verify it', () => {});
+  it('should set the range and verify it', () => {
+    // change range
+    cy.get('@rating-filter').invoke('val', 4).trigger('change');
 
-  it('should check the checkbox and verify it', () => {});
+    // verify that the label contains the new value
+    cy.get('@rating-filter').siblings('label').contains('4');
+  });
 
-  it('should select an option from the select and verify it', () => {});
+  it('should check the checkbox and verify it', () => {
+    cy.get('input[type="checkbox"]')
+      // get the first checkbox
+      .first()
+      .then(($checkbox) => {
+        // check checkbox
+        cy.wrap($checkbox).check();
+
+        // verify that it is checked
+        cy.wrap($checkbox).should('be.checked');
+      });
+  });
+
+  it('should select an option from the select and verify it', () => {
+    cy.get('@restaurant-filter')
+      .children()
+      // get the last menu option
+      .last()
+      // get its text content
+      .invoke('text')
+      .then((text) => {
+        // select option using text
+        cy.get('@restaurant-filter').select(text);
+
+        // verify that the filter shows the selected value
+        cy.get('@restaurant-filter').should('have.value', text);
+      });
+  });
 });
