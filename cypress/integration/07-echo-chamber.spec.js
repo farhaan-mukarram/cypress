ce03cfec4f4eeb7a91d84787d699bdb21b8dec25
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-10-14 23:58:45 +0500

    refactor: move solution for exercise 7 to exercise 8

--- cypress/integration/07-echo-chamber.spec.js
+++ cypress/integration/07-echo-chamber.spec.js
@@ -32,29 +32,3 @@ describe('Initial Page', () => {
     cy.location('pathname').should('contain', 'sign-up');
   });
 });
-
-describe('Sign Up', () => {
-  beforeEach(() => {
-    cy.visit('/echo-chamber/sign-up');
-    cy.get('[data-test="sign-up-email"]').as('sign-up-input');
-    cy.get('[data-test="sign-up-password"]').as('password-input');
-    cy.get('[data-test="sign-up-submit"]').as('sign-up-btn');
-  });
-
-  it('should require an email', () => {
-    cy.get('@sign-up-btn').click();
-    cy.get('@sign-up-input').invoke('prop', 'validity').its('valueMissing').should('be.true');
-  });
-
-  it('should require that the email actually be an email address', () => {
-    cy.get('@sign-up-input').type('test');
-    cy.get('@sign-up-btn').click();
-    cy.get('@sign-up-input').invoke('prop', 'validity').its('typeMismatch').should('be.true');
-  });
-
-  it('should require a password when the email is present', () => {
-    cy.get('@sign-up-input').type('test@123.com');
-    cy.get('@sign-up-btn').click();
-    cy.get('@password-input').invoke('prop', 'validity').its('valueMissing').should('be.true');
-  });
-});
