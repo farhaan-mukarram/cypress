a308d3e6a79a62ee98677731ac2dab9a866e295a
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-03 16:37:49 +0500

    feat: add solution for exercise 14

--- cypress/integration/14-echo-chamber-stubbed-completed.spec.js
+++ cypress/integration/14-echo-chamber-stubbed-completed.spec.js
@@ -8,70 +8,95 @@ export const encodeToken = (token) => Buffer.from(JSON.stringify(token)).toStrin
 describe('Signing in with a seeded database', () => {
   beforeEach(() => {
     cy.setCookie('jwt', encodeToken({ id: 1, email: 'first@example.com' }));
-
     cy.intercept('GET', '/echo-chamber/api', { fixture: 'posts' }).as('postsApi');
     cy.intercept('GET', /\/echo-chamber\/api\/\d+/, { fixture: 'post' }).as('postApi');
     cy.intercept('GET', '/echo-chamber/api/users', { fixture: 'users' }).as('usersApi');
 
+    cy.intercept('POST', '/echo-chamber/api', {
+      statusCode: 201,
+      body: {
+        post: {
+          id: 401,
+          content: 'Recently created post',
+          createdAt: '2021-12-17T13:12:18.418Z',
+          authorId: 220,
+        },
+      },
+    }).as('createPostApi');
+
     cy.visit('/echo-chamber/posts');
+
+    cy.getData('post-create-content-input').as('newPostInput');
+    cy.getData('post-create-submit').as('newPostSubmit');
+    cy.getData('post-preview-list').find('article').as('previews');
 
     cy.fixture('posts').then(({ posts }) => cy.wrap(posts[0]).as('firstPost'));
   });
 
-  /// <reference types="cypress" />
-
-  import '../support/commands-complete';
-
-  export const decodeToken = (token) => JSON.parse(Buffer.from(token, 'base64').toString('utf-8'));
-  export const encodeToken = (token) => Buffer.from(JSON.stringify(token)).toString('base64');
-
-  describe('Signing in with a seeded database', () => {
-    beforeEach(() => {
-      cy.setCookie('jwt', encodeToken({ id: 1, email: 'first@example.com' }));
-
-      cy.intercept('GET', '/echo-chamber/api', { fixture: 'posts' }).as('postsApi');
-      cy.intercept('GET', /\/echo-chamber\/api\/\d+/, { fixture: 'post' }).as('postApi');
-      cy.intercept('GET', '/echo-chamber/api/users', { fixture: 'users' }).as('usersApi');
-
-      cy.intercept('POST', '/echo-chamber/api', {
-        statusCode: 201,
-        body: {
-          post: {
-            id: 401,
-            content: 'Recently created post',
-            createdAt: '2021-12-17T13:12:18.418Z',
-            authorId: 220,
-          },
-        },
-      }).as('createPostApi');
-
-      cy.visit('/echo-chamber/posts');
-
-      cy.getData('post-create-content-input').as('newPostInput');
-      cy.getData('post-create-submit').as('newPostSubmit');
-      cy.getData('post-preview-list').find('article').as('previews');
-
-      cy.fixture('posts').then(({ posts }) => cy.wrap(posts[0]).as('firstPost'));
+  it('should render the posts from the API', () => {
+    cy.fixture('posts').then(({ posts }) => {
+      cy.get('@previews').should('have.length', posts.length);
     });
+  });
 
-    it('should render the posts from the API', () => {
-      cy.fixture('posts').then(({ posts }) => {
-        cy.get('@previews').should('have.length', posts.length);
+  it('should navigate to the URL of the post that you clicked on', () => {
+    cy.get('@previews')
+      .first()
+      .as('firstPostLink')
+      .parent()
+      .then(($parentElem) => {
+        const link = $parentElem.attr('href');
+
+        cy.get('@firstPostLink').click();
+        cy.wait('@postApi');
+
+        cy.location('pathname').should('contain', link);
       });
+  });
+
+  it('should send a POST request when submitting the form', () => {
+    cy.get('@newPostInput').type('Testing hot takes{enter}');
+
+    cy.wait('@createPostApi').its('request.method').should('eq', 'POST');
+  });
+
+  describe('An individual post', () => {
+    beforeEach(() => {
+      cy.intercept('PATCH', '/echo-chamber/api/1', {
+        statusCode: 201,
+      }).as('updatePostApi');
+
+      cy.intercept('DELETE', '/echo-chamber/api/1', {
+        statusCode: 201,
+      }).as('deletePostApi');
+
+      cy.visit('/echo-chamber/posts/1');
+
+      cy.get('[data-test="post-detail"]').as('individualPostPreview');
+
+      cy.get('[data-test="post-detail-controls-edit-button"]').as('postEditButton');
+
+      cy.get('[data-test="post-detail-controls-delete-button"]').as('postDeleteButton');
     });
 
-    it('should navigate to the URL of the post that you clicked on', () => {});
+    it('should show an edit field when you click on the edit button', () => {
+      cy.get('@postEditButton').click();
 
-    it('should send a POST request when submitting the form', () => {});
+      cy.get('[data-test="post-detail-draft-content"]');
+    });
 
-    describe('An individual post', () => {
-      beforeEach(() => {});
+    it('should send a PATCH request when you send your edit', () => {
+      cy.get('@postEditButton').click();
 
-      it('should show an edit field when you click on the edit button', () => {});
+      cy.get('[data-test="post-detail-draft-content"]').type('post test edit{enter}');
 
-      it('should send a PATCH request when you send your edit', () => {});
+      cy.wait('@updatePostApi').its('request.method').should('eq', 'PATCH');
+    });
 
-      it('should send a DELETE request when click on the delete button', () => {});
+    it('should send a DELETE request when click on the delete button', () => {
+      cy.get('@postDeleteButton').click();
+
+      cy.wait('@deletePostApi').its('request.method').should('eq', 'DELETE');
     });
   });
 });
